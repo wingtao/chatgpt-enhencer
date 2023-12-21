7500afab405d153ffd848ccec43502542cab9a3c
liyuntao <liyuntao@kuaishou.com>
2023-12-21 21:04:07 +0800

    feat: 支持黑暗模式和隐藏

--- content.ts
+++ content.ts
@@ -8,9 +8,53 @@ export const config: PlasmoCSConfig = {
   matches: ["https://chat.openai.com/*"]
 }
 
+const debounce = (fn: (...arg: any) => void, timeout: number) => {
+  let timer = null
+  return (...arg: any) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => fn(arg), timeout)
+  }
+}
+
 const ID_KEY = "data-message-id"
 
 let container: HTMLDivElement | null
+let switcherContainer: HTMLDivElement | null
+const destroy = () => {
+  if (container) {
+    document.body.removeChild(container)
+    container = null
+  }
+}
+
+const paintSwitcher = () => {
+  switcherContainer = document.createElement("div")
+  switcherContainer.className = "switcher opening"
+  const upper = document.createElement("div")
+  const downer = document.createElement("div")
+  upper.className = "switcher-up"
+  downer.className = "switcher-down"
+  switcherContainer.onclick = () => {
+    if (container.className.indexOf("opening") > -1) {
+      container.className = container.className.replace("opening", "closed")
+      switcherContainer.className = switcherContainer.className.replace(
+        "opening",
+        "closed"
+      )
+    } else {
+      container.className = container.className.replace("closed", "opening")
+      switcherContainer.className = switcherContainer.className.replace(
+        "closed",
+        "opening"
+      )
+    }
+  }
+  switcherContainer.appendChild(upper)
+  switcherContainer.appendChild(downer)
+  document.body.appendChild(switcherContainer)
+}
 
 const paintFixedPanel = (param: { id: string; name: string }[]) => {
   if (!param?.length) {
@@ -20,7 +64,7 @@ const paintFixedPanel = (param: { id: string; name: string }[]) => {
     container.innerHTML = ""
   } else {
     container = document.createElement("div")
-    container.className = "locator-container"
+    container.className = "locator-container opening"
   }
   const header = document.createElement("h2")
   header.innerText = "Question locator"
@@ -52,30 +96,51 @@ const paintFixedPanel = (param: { id: string; name: string }[]) => {
   document.body.appendChild(container)
 }
 
-const onDocLoad = () => {
+const onDocLoad = debounce(() => {
   const questionEles = document.querySelectorAll(
     `[${ID_KEY}][data-message-author-role="user"]`
   )
+  if (!questionEles.length) {
+    destroy()
+    return
+  }
   const questionIds = Array.from(questionEles).map((ele: HTMLElement) => ({
     id: ele.getAttribute(ID_KEY),
     name: ele.innerText
   }))
   paintFixedPanel(questionIds)
+}, 1000)
+
+let observer: MutationObserver | null = null
+const listenContentChange = () => {
+  // 停止观察旧的
+  if (observer) {
+    observer.disconnect()
+  }
+  const targetNode = document.querySelector("main")
+
+  // 观察器的配置（需要观察什么变动）
+  const config = { attributes: false, childList: true, subtree: true }
+  // 当观察到变动时执行的回调函数
+  const callback = function (mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    onDocLoad()
+  }
+
+  // 创建一个观察器实例并传入回调函数
+  observer = new MutationObserver(callback)
+  // 以上述配置开始观察目标节点
+  observer.observe(targetNode, config)
 }
 
 const checkIsLoad = () => {
   if (document.querySelector("main")) {
-    setTimeout(onDocLoad, 3000)
+    paintSwitcher()
+    listenContentChange()
   } else {
     setTimeout(() => {
       checkIsLoad()
     }, 1000)
   }
 }
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "TabUpdated") {
-    // 执行相关操作
-    checkIsLoad()
-  }
-})
+checkIsLoad()
